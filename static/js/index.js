0e4019a085219fa9b1c62a62862f0fa8a1076170
cjun714 <cjun714@gmail.com>
2021-01-26 23:49:16 +0800

    update vuejs-paginate to 2.1.0

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,16 @@
-fetch('/api/comics/1')
+page = 1
+
+idx = getParameters()["page"]
+if (idx != null) {
+  page = idx
+}
+
+fetch('/api/comics/' + page)
   .then(response => response.json())
   .then(data => {
     v.$refs.list.comics = data.data
     v.$refs.index.pageCount = data.count / 20
+    v.$refs.index.page = page
   })
   .catch(error => console.log(error));
 
@@ -24,28 +32,38 @@ Vue.component('comic-list', {
 })
 
 Vue.component('paginate', VuejsPaginate)
+
 Vue.component('index', {
   template: `
-    <paginate :pageCount="pageCount" :page-range="10" :margin-pages="1" :containerClass="'pagination'"
+    <paginate v-model="page" :pageCount="pageCount" :page-range="10" :margin-pages="1" :containerClass="'pagination'"
       :clickHandler="clickCallback"></paginate>
   `,
   data: function () {
     return {
-      pageCount: 0
+      pageCount: 0,
+      page: 1
     }
   },
+  created: function () {},
   methods: {
     clickCallback: function (page) {
-      url = '/api/comics/'
-      fetch(url + page)
-        .then(response => response.json())
-        .then(data => {
-          v.$refs.list.comics = data.data
-          v.$refs.index.pageCount = data.count / 20
-        })
-        .catch(error => console.log(error));
+      // url = '/api/comics/'
+      window.location.href = '/page/index.html' + '?page=' + page
+      // fetch(url + page)
+      //   .then(response => response.json())
+      //   .then(data => {
+      //     v.$refs.list.comics = data.data
+      //     v.$refs.index.pageCount = data.count / 20
+      //   })
+      //   .catch(error => console.log(error));
     }
   }
 })
 
-function clickCallback(page) {}
+function getParameters() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+    vars[key] = value;
+  });
+  return vars;
+}
